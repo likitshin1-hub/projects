34496b6dc8a760a2df758099601a1fcf661f5864
likitshin1-hub <likit.shin1@gmail.com>
2026-08-07 12:59:36 +0700

    Fix DioClient HTTP methods

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -10,4 +10,56 @@ class DioClient {
   }
 
   Dio get client => dio;
+
+
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return dio.get(
+      path,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.post(
+      path,
+      data: data,
+      options: options,
+    );
+  }
+
+
+  Future<Response> put(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.put(
+      path,
+      data: data,
+      options: options,
+    );
+  }
+
+
+  Future<Response> delete(
+    String path, {
+    dynamic data,
+    Options? options,
+  }) {
+    return dio.delete(
+      path,
+      data: data,
+      options: options,
+    );
+  }
 }
